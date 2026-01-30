ec571a487dd8dda139ffde48fba380b442ce358e
bonny <128207489+bonaktan@users.noreply.github.com>
2026-01-30 22:14:57 +0800

    styling of top part

--- app/home/main.tsx
+++ app/home/main.tsx
@@ -1,43 +1,56 @@
 import React from "react";
+import leejeans from "/leejeans.jpg"
+interface WorksProps {
+    name: string;
+    description: string;
+    // add other props here
+}
 
-function Title() {
+const Title = () => {
     return (
-        <div>
-            <p>hi, im bonny</p>
-            <p>i create unreadable literature</p>
-            
+        <div className="text-center">
+            <span className="flex justify-center text-7xl"><p className="">hi, im&nbsp;</p><p className="text-primary-300">bonny</p></span>
+            <p className="text-5xl">i do random things</p>
+            <p>this is still a wip oki :)</p>
         </div>
     );
 }
-function Contact() {
+const Contact = () => {
     return (
-        <div>
-            <p>contact:</p>
-            <p>mail: bon@bonnybonnybonaktan.xyz</p>
-            <p>github: bonaktan</p>
-            <p>discord: bonnyyyy_tan</p>
-            <p>roblox: @bonnybonnybonaktan</p>
+        <div className="flex ml-70 mr-70 text-center">
+            <p className="basis-1/4">mail: bon@bonnybonnybonaktan.xyz</p>
+            <p className="basis-1/4">github: bonaktan</p>
+            <p className="basis-1/4">discord: bonnyyyy_tan</p>
+            <p className="basis-1/4">roblox: @bonnybonnybonaktan</p>
         </div>
-    )
+    );
 }
-function Works() {
+const Works:React.FC<WorksProps> = ({ name, description }) => {
     return (
         <div>
-            <p>Title</p>
-            <p>Description</p>
+            <p>{name}</p>
+            <p>{description}</p>
         </div>
-    )
+    );
 }
-export function Main() {
+export const Main = () => {
     return (
         <div>
-            <Title />
-            <Contact/>
-            <div>
-                <p>works:</p>
-                <Works />
+            <div className="pt-25">
+                <Title />
+                <Contact />
             </div>
-            <p>message of the push: 君が好きよ、 今もずっと好きよ。</p>
+            <div className="mt-10">
+                <p>--- i still hadn&apos;t gotten around to styling this part :3 ---</p>
+                <p>works:</p>
+                <Works name="Realistic Boiling Water Reactor Simulator - Quality Assurance" description="hey developer, we reached -65535 aprm" />
+                <Works name="ENGRISH - Head Developer" description="fix your engrish with engrish so that it turns to english" />
+                <Works name="ITskolarSystem - Backend Developer, System Admin" description='a custom bot that we like to describe as "shit"' />
+            </div>
+            <div className="flex mt-2">
+                <p>message of the push:</p>
+                <img src={leejeans} className="ml-1 w-32"/>
+            </div>
         </div>
     );
 }
--- app/root.tsx
+++ app/root.tsx
@@ -14,7 +14,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
                 <Meta />
                 <Links />
             </head>
-            <body>
+            <body className="font-inconsolata">
                 {children}
                 <ScrollRestoration />
                 <Scripts />
