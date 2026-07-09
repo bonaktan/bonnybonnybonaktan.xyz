f9f95c73fe7aedf0b5553531072d8c9bb064ef15
bonaktan <bon@bonnybonnybonaktan.xyz>
2026-07-09 20:43:46 +0800

    Updated Deployment + Changes in Content

--- app/home/main.tsx
+++ app/home/main.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import leejeans from "/leejeans.jpg"
 interface WorksProps {
     name: string;
     description: string;
@@ -18,10 +17,10 @@ const Title = () => {
 const Contact = () => {
     return (
         <div className="flex ml-70 mr-70 text-center">
-            <p className="basis-1/4">mail: bon@bonnybonnybonaktan.xyz</p>
-            <p className="basis-1/4">github: bonaktan</p>
-            <p className="basis-1/4">discord: bonnyyyy_tan</p>
-            <p className="basis-1/4">roblox: @bonnybonnybonaktan</p>
+            <p className="basis-1/4">mail: <a href="mailto:bon@bonnybonnybonaktan.xyz">bon@bonnybonnybonaktan.xyz</a></p>
+            <p className="basis-1/4">github: <a href="https://github.com/bonaktan/">bonaktan</a></p>
+            <p className="basis-1/4 flex flex-col"><p>discord: bonnybonnybon</p> <p>(userid: 860785995484758037)</p></p>
+            <p className="basis-1/4">roblox: <a href="https://www.roblox.com/users/5722052356/profile">@bonnybonnybonaktan</a></p>
         </div>
     );
 }
@@ -42,14 +41,27 @@ export const Main = () => {
             </div>
             <div className="mt-10">
                 <p>--- i still hadn&apos;t gotten around to styling this part :3 ---</p>
-                <p>works:</p>
-                <Works name="Realistic Boiling Water Reactor Simulator - Quality Assurance" description="hey developer, we reached -65535 aprm" />
-                <Works name="ENGRISH - Head Developer" description="fix your engrish with engrish so that it turns to english" />
-                <Works name="ITskolarSystem - Backend Developer, System Admin" description='a custom bot that we like to describe as "shit"' />
+                <div className="p-2">
+                    <p>current works:</p>
+                    <Works name="[secret game :3] - Tester" description="does it exist? yes!" />
+                    <Works name="ITskolarSystem - Backend Developer, System Admin" description='a custom bot that we like to describe as "shit"' />
+                </div>
+                <div className="p-2">
+                    <p>past works:</p>
+                    <Works name="Realistic Boiling Water Reactor Simulation Community - Quality Assurance (Nov 2025 - Apr 2026)" description="hey developer, we reached -65535 aprm" />
+                    {/* <Works name="Realistic Boiling Water Reactor Simulation Community - Technical Department (Apr 2026)" description="vibecoded clankers? hellyeaaaaaaaaaa" /> */ /* i am not sure if this should be included ngl */ }
+                    {/* <Works name="Deep Space Tycoon:Reborn - Quality Assurance (Jan 2026 - Feb 2026)" description="press button yes press button yes, ooo munni!" /> */}
+                    <Works name="ENGRISH - Head Developer" description="fix your engrish with engrish so that it turns to english (Sep 2024 - Mar 2025)" />
+                </div>
+                <p className="p-2">
+                    <p>Projects and Commissions</p>
+                    <Works name="Pearl Horizon Airlines" description="part 1 of 2 on my airplane series" />
+                    <Works name="Skybridge Airways" description="part 2 of 2 on my airplane series, now on c++" />
+                    <Works name="Correct Responser" description="i am both too lazy and too workaholic to response 17 papers at ONCE." />
+                </p>
             </div>
             <div className="flex mt-2">
-                <p>message of the push:</p>
-                <img src={leejeans} className="ml-1 w-32"/>
+                <p>message of the push: hehe, dunce &lt; :3</p>
             </div>
         </div>
     );
